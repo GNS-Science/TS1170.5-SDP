a5067fa20a26941973c9c116fe542d12d4a96a67
Anne Hulsey <anne.hulsey@auckland.ac.nz>
2024-10-10 12:50:28 +1300

    remove no longer relevant, commented lines

--- nzssdt_2023/scripts/create_parameter_tables.py
+++ nzssdt_2023/scripts/create_parameter_tables.py
@@ -57,5 +57,3 @@ with open(sa_path, "rb") as file:
 dm_df = dm_gen.create_D_and_M_table(site_list, APoEs)
 dm_df.to_csv(dm_path)
 
-# print(f"{filename} saved to \n\t{os.getcwd()}")
-# (writes the D&M csv files)
