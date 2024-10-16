a32893e569a280dead5dd4d2207c394d18e4f498
Anne Hulsey <anne.hulsey@auckland.ac.nz>
2024-10-17 12:31:33 +1300

    tmp workaround for Windows

--- nzssdt_2023/config.py
+++ nzssdt_2023/config.py
@@ -11,7 +11,6 @@ from pathlib import PurePath
 RESOURCES_FOLDER = str(PurePath(os.path.realpath(__file__)).parent.parent / "resources")
 
 WORKING_FOLDER = os.getenv("WORKING_FOLDER", tempfile.gettempdir())
-"""A standardised directory path for disposable working files."""
 
 
 
