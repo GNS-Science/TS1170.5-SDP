2c725c16df5091619cc247d8c24f97b13c1f2bf2
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-10-21 10:37:07 +1300

    default config.WORKING_FOLDER uses platform independent tmp folder;

--- nzssdt_2023/config.py
+++ nzssdt_2023/config.py
@@ -5,15 +5,15 @@ Configuration module for the library.
 NB: most users will never need this. It's mainly  for NSHM developers.
 """
 import os
+import tempfile
+
 from pathlib import PurePath
 
 RESOURCES_FOLDER = str(PurePath(os.path.realpath(__file__)).parent.parent / "resources")
 
-WORKING_FOLDER = os.getenv("WORKING_FOLDER", "tmp")  # /tmp doesn't work in Windows.
+WORKING_FOLDER = os.getenv("WORKING_FOLDER", tempfile.gettempdir())
 """A standardised directory path for disposable working files."""
-# I had to change the above from /tmp to tmp and create a folder in scripts named tmp to get it to work.
-# Need an automated, disposable solution that also works on Windows.
-# https://stackoverflow.com/questions/15111103/tmp-folder-in-windows-like-tmp-in-linux
+
 
 DISAGG_HAZARD_ID = "NSHM_v1.0.4_mag"
 """Disaggregations for calculation of mean magnitude were done for magnitude only (rather than mag,
