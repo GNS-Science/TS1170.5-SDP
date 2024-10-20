ce8a20d40c99e656612c18b72a8843aff1067af4
Chris DiCaprio <christopher.dicaprio@gmail.com>
2024-10-21 09:55:37 +1300

    Mean mag test (#37)
    * helper function to read mean mag dataframe; improve test coverage
    * linting
    * correct docstring
    * non-legacy magnitude rounding by default
    * test legacy and non legacy magnitudes; test read/write magnitude df
    * efficient use of mean mag csv files as cache
    * mock THS query for testing

--- nzssdt_2023/data_creation/constants.py
+++ nzssdt_2023/data_creation/constants.py
@@ -3,37 +3,36 @@ This module contains constants used for mean mag table generation. The intermedi
 and probabilities, regardless of what is requested by the call to extract_m_values.
 """
 
-from pathlib import Path
+from typing import Any, Dict
 
-from nzshm_common.grids import load_grid
-from nzshm_common.location import CodedLocation
-from nzshm_common.location.location import LOCATION_LISTS
+from nzshm_common.location.location import LOCATION_LISTS, location_by_id
 from toshi_hazard_store.model import ProbabilityEnum
 
-from nzssdt_2023.config import WORKING_FOLDER
-from nzssdt_2023.data_creation.mean_magnitudes import lat_lon_from_id
+
+def lat_lon_from_id(id):
+    return (location_by_id(id)["latitude"], location_by_id(id)["longitude"])
+
 
 location_list = "SRWG214"
 location_grid = "NZ_0_1_NB_1_1"
 akl_location_id = "srg_29"
 
-SRWG_LOCATIONS = [
-    CodedLocation(*lat_lon_from_id(_id), 0.001)
+RP_TO_POE = {
+    10000: ProbabilityEnum._05_PCT_IN_50YRS,
+    5000: ProbabilityEnum._1_PCT_IN_50YRS,
+    2500: ProbabilityEnum._2_PCT_IN_50YRS,
+    1000: ProbabilityEnum._5_PCT_IN_50YRS,
+    500: ProbabilityEnum._10_PCT_IN_50YRS,
+    250: ProbabilityEnum._18_PCT_IN_50YRS,
+    100: ProbabilityEnum._39_PCT_IN_50YRS,
+    50: ProbabilityEnum._63_PCT_IN_50YRS,
+    25: ProbabilityEnum._86_PCT_IN_50YRS,
+}
+POE_TO_RP = {poe: rp for rp, poe in RP_TO_POE.items()}
+
+ALL_SITES: Dict[str, Dict[str, Any]] = {
+    location_by_id(_id)["name"]: location_by_id(_id)
     for _id in LOCATION_LISTS[location_list]["locations"]
-]
-GRID_LOCATIONS = [CodedLocation(*loc, 0.001) for loc in load_grid(location_grid)]
-AKL_LOCATIONS = [CodedLocation(*lat_lon_from_id(akl_location_id), 0.001)]
-POES = [
-    ProbabilityEnum._2_PCT_IN_50YRS,
-    ProbabilityEnum._5_PCT_IN_50YRS,
-    ProbabilityEnum._10_PCT_IN_50YRS,
-    ProbabilityEnum._18_PCT_IN_50YRS,
-    ProbabilityEnum._39_PCT_IN_50YRS,
-    ProbabilityEnum._63_PCT_IN_50YRS,
-    ProbabilityEnum._86_PCT_IN_50YRS,
-]
+}
 
-
-SRWG_214_MEAN_MAG_FILEPATH = Path(WORKING_FOLDER, "SRWG214_mean_mag.csv")
-GRID_MEAN_MAG_FILEPATH = Path(WORKING_FOLDER, "grid_mean_mag.csv")
-AKL_MEAN_MAG_P90_FILEPATH = Path(WORKING_FOLDER, "AKL_90pct_mean_mag.csv")
+DEFAULT_RPS = [25, 50, 100, 250, 500, 1000, 2500]
